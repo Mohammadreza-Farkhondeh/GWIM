d77af5404c302815d439620bfb09031873aaa624
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2024-05-17 18:46:59 +0330

    added more network generators

--- src/utils.py
+++ src/utils.py
@@ -27,9 +27,49 @@ def get_network(n: str) -> Network:
             print(f"Key {e} did not found.")
         except Exception as e:
             print(f"An error occurred: {e}")
+    elif n == "barbasi":
+        graph = nx.barabasi_albert_graph(100, 5)
+        network = Network(graph=graph)
+    elif n == "watts":
+        graph = nx.watts_strogatz_graph(100, 5, 0.33)
+        network = Network(graph=graph)
     else:
-        nodes = [1, 2, 3, 4, 5]
-        edges = [(1, 2), (1, 3), (2, 4), (3, 4), (4, 5)]
-        network = Network(nodes=nodes, edges=edges)
+        nodes = list(range(1, 17))
+        edges = [
+            (1, 2),
+            (1, 3),
+            (1, 4),
+            (1, 5),
+            (2, 3),
+            (2, 6),
+            (3, 7),
+            (4, 5),
+            (4, 8),
+            (5, 6),
+            (5, 9),
+            (6, 7),
+            (6, 10),
+            (7, 8),
+            (7, 9),
+            (7, 10),
+            (7, 11),
+            (8, 9),
+            (8, 10),
+            (9, 10),
+            (10, 11),
+            (10, 12),
+            (12, 13),
+            (12, 14),
+            (12, 15),
+            (12, 16),
+            (13, 14),
+            (14, 15),
+            (14, 16),
+            (15, 16),
+        ]
+        graph = nx.Graph()
+        graph.add_nodes_from(nodes)
+        graph.add_edges_from(edges)
+        return Network(graph=graph)
 
     return network
